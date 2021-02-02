2aeee9d026ee5c2325453ab6e9914eedc2e6f9f2
MelonDang <melondang3575@gmail.com>
2021-02-02 19:43:25 +0900

    Add Shoot bullet

--- TextFlightGame/console.h
+++ TextFlightGame/console.h
@@ -1,7 +1,7 @@
 #include <windows.h>
 
-void SetConsolePos(short x, short y);
-COORD GetConsolePos();
+void SetConsoleCursorPos(short x, short y);
+COORD GetConsoleCursorPos();
 void SetConsoleColor(unsigned char bg_color, unsigned char text_color);
 void SetShowCursor(bool flag);
 void SetConsoleSize(int cols, int lines);
--- TextFlightGame/main.cpp
+++ TextFlightGame/main.cpp
@@ -8,6 +8,10 @@ int main()
 	int plane_x = 14;
 	int plane_y = 28;
 
+	int bullet_x = 0;
+	int bullet_y = 0;
+	bool is_bullet_exist = false;
+
 	SetConsoleSize(30, 30);
 
 	while (true)
@@ -23,7 +27,23 @@ int main()
 			plane_x += 1;
 		}
 
-		SetConsolePos(plane_x, plane_y);
+		if (!is_bullet_exist && GetAsyncKeyState(VK_SPACE) & 0x8000)
+		{
+			bullet_x = plane_x;
+			bullet_y = plane_y - 1;
+			is_bullet_exist = true;
+		}
+
+		if (is_bullet_exist)
+		{
+			SetConsoleCursorPos(bullet_x, bullet_y);
+			printf("ก่");
+			bullet_y -= 1;
+
+			if (bullet_y <= 0) { is_bullet_exist = false; }
+		}
+
+		SetConsoleCursorPos(plane_x, plane_y);
 		printf("กฺ");
 		Sleep(50);
 	}
